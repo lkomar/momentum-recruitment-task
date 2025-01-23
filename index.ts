ea73da06df871c66fece672e5efcd92f292101d6
Lukasz Komar <8258799+lkomar@users.noreply.github.com>
2025-01-23 21:50:25 +0100

    feat: Add reducer, present store usage, showcase subscribing & unsubscribing

---
+++ index.ts
@@ -0,0 +1,30 @@
+import { Action, createStore } from './store';
+
+enum CounterAction {
+  increment = 'INCREMENT',
+  decrement = 'DECREMENT'
+}
+
+const counterReducer = (state = 0, action: Action) => {
+  switch (action.type) {
+    case CounterAction.increment:
+      return state + 1;
+    case CounterAction.decrement:
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
+const store = createStore(counterReducer, 0);
+
+const exampleListener = () => {
+  console.log('Nowy stan:', store.getState());
+}
+
+const unsubscribe = store.subscribe(exampleListener);
+
+store.dispatch({ type: CounterAction.increment });
+store.dispatch({ type: CounterAction.decrement });
+
+unsubscribe();
